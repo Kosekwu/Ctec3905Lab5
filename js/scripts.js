ebd17fa3f46d3d6deec7cb190e2395bf5e48db28
Kose <cosykose@hotmail.co.uk>
2018-11-12 17:59:53 +0000

    Modify an interface element using a color input

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ const theDate = document.getElementById("date");
 const getDate = document.querySelector('input[type="date"]');
 const colour = document.getElementById("color");
 const value = document.getElementById("the-value");
+const Range = document.getElementById("range");
 
 var d = document.getElementById("date").innerText;
 /*store h1 innertext */
@@ -14,7 +15,7 @@ var originaltext = document.getElementById("date").innerText;
 function showDate(){
   /*new date value is stored in d */
   let d = new Date(getDate.value);
-  alert((d.getDate()) + '/' + d.getMonth() + '/' +  d.getFullYear());
+  /*alert((d.getDate()) + '/' + d.getMonth() + '/' +  d.getFullYear());*/
   d.toDateString(); /*make date human readable */
 
 /* add original text and date and display them*/
@@ -29,3 +30,8 @@ var aa =  value.style.backgroundColor = colour.value;
   console.log(aa);
 }
 theForm.addEventListener("input", setColor);
+
+function rangeAction{
+
+}
+Range.addEventListener("input", rangeAction);
